d7a1ffb11a3169b271474776610892efd0dceac4
Stanislav <stanislav.nikunenko@gmail.com>
2021-06-16 14:01:40 +0300

    EntityManagerFactory unwrap

--- src/main/java/dao/HibernateSessionFactory.java
+++ src/main/java/dao/HibernateSessionFactory.java
@@ -6,6 +6,9 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 @Component
 public class HibernateSessionFactory {
     private static SessionFactory sessionFactory;
@@ -17,17 +20,17 @@ public class HibernateSessionFactory {
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
-                Configuration configuration = new Configuration().configure();
+//                Configuration configuration = new Configuration().configure();
+//                // Добавить класс @Entity сюда и в hibernate.cfg.xml добавить Mapping
+//                configuration.addAnnotatedClass(Topic.class);
+//                configuration.addAnnotatedClass(Test.class);
+//                configuration.addAnnotatedClass(Question.class);
+//                configuration.addAnnotatedClass(Literature.class);
+//                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+//                sessionFactory = configuration.buildSessionFactory(builder.build());
 
-                // Добавить класс @Entity сюда и в hibernate.cfg.xml добавить Mapping
-                configuration.addAnnotatedClass(Topic.class);
-                configuration.addAnnotatedClass(Test.class);
-                configuration.addAnnotatedClass(Question.class);
-                configuration.addAnnotatedClass(Literature.class);
-
-                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties());
-                sessionFactory = configuration.buildSessionFactory(builder.build());
+                EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence-unit");
+                sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
             } catch (Exception ex) {
                 throw new RuntimeException(ex);
             }
